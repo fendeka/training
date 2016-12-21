c914485099d8d12c744a02e3b0e11ce83985a013
fendeka <fendeka@mail.ru>
2016-12-20 22:04:13 -0500

    структура

---
+++ app/config.php
@@ -0,0 +1,11 @@
+<?php
+$settings = array(
+    'database' => array(
+        'driver' => 'MySQLDriver',
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => '1515',
+        'dbname' => 'training',
+        'type' => 'mysql'
+    )
+);
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 echo hello;
+
+require_once("app/config.php");
+
+Application::setConfig($settings);
+
 ?>
